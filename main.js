9284935f89cacd03a78442ba865b770ba5a5e6f5
VanDu97 <nguyenvandu199740@gmail.com>
2019-01-28 12:25:04 +0700

    First Solution

---
+++ main.js
@@ -0,0 +1,32 @@
+
+/**
+ * Given an integer n, your task is to check if the sum of the prime factors of n is a prime.
+ * For n = 148, the output should be primeSumOfPrimeFactors(n) = true.
+ * 148 has prime factors 2, 2 and 37. Their sum is 41, which is prime, so the answer is true.
+ * For n = 8, the output should be primeSumOfPrimeFactors(n) = false.
+ * 8 has prime factors 2, 2, and 2, which sum to 6. Since 6 isn't a prime (6 = 2 * 3), the answer is false.
+ */
+primeSumOfPrimeFactors = n => {
+  var array = [];
+  var index = 2;
+  while (n > 1) {
+    if (n % index == 0) {
+      array.push(index);
+      n = n / index;
+    } else {
+      index++;
+    }
+  }
+  return check(array.reduce((x, y) => x + y));
+};
+
+check = n => {
+  for (i = 2; i * i <= n; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+console.log(primeSumOfPrimeFactors(148));
